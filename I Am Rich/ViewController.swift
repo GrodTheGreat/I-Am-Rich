00f5e1d0a5ee9b1caa30a033497817ff90a0b75c
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2024-12-24 23:52:20 -0500

    Add files via upload
    Complete Section

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Gerard  Causse on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
